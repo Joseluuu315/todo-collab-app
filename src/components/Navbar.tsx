db18fef471e2fcac5ba34e8bd150d9e6d4804bcd
José Luis Fuentes Parra <josefnts3115@gmail.com>
2025-06-17 16:01:40 +0200

    feat: added login and register in header

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,6 +6,10 @@ const MyNavbar: React.FC = () => {
     <Navbar bg="dark" variant="dark">
       <Container>
         <Navbar.Brand href="#">To-Do Colaborativa</Navbar.Brand>
+        {/* Login */}
+        <Navbar.Brand href="#">Login</Navbar.Brand>
+        {/* Register */}
+        <Navbar.Brand href="#">Register</Navbar.Brand>
       </Container>
     </Navbar>
   );
