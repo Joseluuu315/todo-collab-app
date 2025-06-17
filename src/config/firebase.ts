168a36ca513917cd6885516a95c0f871ecf81e97
José Luis Fuentes Parra <josefnts3115@gmail.com>
2025-06-17 16:04:27 +0200

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,15 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth"; 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB1EdZrP6q8c2HjYwRM--6TDV6AQoVwV90",
-  authDomain: "todo-database-1d7a7.firebaseapp.com",
-  projectId: "todo-database-1d7a7",
-  storageBucket: "todo-database-1d7a7.appspot.com", 
-  messagingSenderId: "858078294150",
-  appId: "1:858078294150:web:eb93cbaa463f8b1c266107",
-  measurementId: "G-NSKGJ527CY",
-};
+
 
 const app = initializeApp(firebaseConfig);
 
