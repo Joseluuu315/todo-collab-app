02b74e01fbeda35ce3dd05c0de6e688c7e1d3cfb
José Luis Fuentes Parra <josefnts3115@gmail.com>
2025-06-17 16:01:53 +0200

    feat: added new firebase system

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,17 +1,20 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth"; 
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_BUCKET",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID",
+  apiKey: "AIzaSyB1EdZrP6q8c2HjYwRM--6TDV6AQoVwV90",
+  authDomain: "todo-database-1d7a7.firebaseapp.com",
+  projectId: "todo-database-1d7a7",
+  storageBucket: "todo-database-1d7a7.appspot.com", 
+  messagingSenderId: "858078294150",
+  appId: "1:858078294150:web:eb93cbaa463f8b1c266107",
+  measurementId: "G-NSKGJ527CY",
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const auth = getAuth(app); 
+
+export default app;
