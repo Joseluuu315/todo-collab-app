15ab634fa3abed0855cda7f77fd8ba4aad7724ac
José Luis Fuentes Parra <josefnts3115@gmail.com>
2025-06-18 23:59:06 +0200

    fix: firebase connection

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'todo-collab-app',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'todo-collab-app',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
